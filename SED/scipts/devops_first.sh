a9b63ed61ececc690fb3b28fb636b7c6cfb7d27c
OjaswiSagar <122374723+OjaswiSagar@users.noreply.github.com>
2023-02-05 07:11:51 -0800

    Add files via upload

---
+++ SED/scipts/devops_first.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "I will be a great DevOps Enginner"
+#echo $BASH
+name="Sagar"
+echo "Hello ${name}, please enter your ege"
+read age
+echo "My age is ${age}"
+echo "sub: hello, i am $1"
+sleep 2
+echo "Me: Aapka swagat hain Sagard chennel pe"
+sleep 2
+echo "please subscribe"
---
+++ SED/scipts/if_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ "$1" = "like" ]
+then
+ echo "Hey Please $1 this video"
+else
+ echo "Olay, then please subscribe:)"
+fi
